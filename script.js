b44bc6f8666f6b63c537cbb6dfacaf4377890e20
NuryanaNn <nuryan504@gmail.comescgit clear>
2021-09-14 20:51:56 +0700

    Initial Commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     // Navbar
     $("#love").addClass("animate__animated animate__backInDown animate__delay-1s");
-    $("#jes").addClass("animate__animated animate__slideInLeft animate__delay-2s");
-    $("#yan").addClass("animate__animated animate__slideInRight animate__delay-2s");
+    $("#jes").addClass("animate__animated animate__fadeIn animate__delay-2s");
+    $("#yan").addClass("animate__animated animate__fadeIn animate__delay-2s");
 
     $("#col1").addClass("animate__animated animate__fadeIn animate__delay-1s");
     $("#col2").addClass("animate__animated animate__fadeIn animate__delay-2s");
